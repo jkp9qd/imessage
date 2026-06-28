a5affcba5ea97b5414ca3c5fe38475f2e7b29ece
Jarvis Potter <jarviskpotter@gmail.com>
2026-06-28 02:40:28 -0500

    Add Clerk webhook handler for user events in clerk.webhook.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import { clerkMiddleware } from "@clerk/express"
 import User from "./models/user.model.js"
 import { connectDB } from "./lib/db.js"
 import job from "./lib/cron.js"
+import clerkWebhook from "./webhooks/clerk.webhook.js"
 
 
 const app = express();
@@ -15,6 +16,9 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+// it's important that you don't parse the webhook event data, it should be in the raw format for signature verification
+app.use("api/webhook/clerk", express.raw({type:"application/json"}),clerkWebhook); // for clerk webhook
 
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
